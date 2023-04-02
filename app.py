22f541d24420a136cc66d2817be1b16669f2321b
sorbid <biome@list.ru>
2023-04-02 20:16:33 +0700

    feat: изменена структура проекта
    также пофикшена проблема с совместимостью с sqlalchemy 2.0

--- app.py
+++ app.py
@@ -1,49 +1,17 @@
-from flask import Flask, render_template, url_for, request, redirect, session, jsonify, abort, send_file
-from flask.helpers import send_from_directory
-from sqlalchemy import *
-from sqlalchemy import  create_engine
-from sqlalchemy import exc
-from sqlalchemy.orm import create_session, aliased
-from sqlalchemy.schema import Table, MetaData
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import re
-import math
-import hashlib
-import os
-import time
-import json
-import pathlib
-import platform
-import csv
-import time
-import shutil
+from flask import Flask
+from dotenv import load_dotenv
+from pathlib import Path
+from os import getenv, getcwd
 
+from models.database import init_db
+from routes import pages
 
-if platform.system() == 'Windows':
-    engine = create_engine('mysql+pymysql://root:@localhost:3306/projectsystem')
-    FULL_PATH_FOLDER = pathlib.Path(os.getcwd(), 'static', 'files')
-conn = engine.connect()
-metadata = MetaData(bind=engine)
-UPLOAD_FOLDER = pathlib.Path('/static', 'files')
-
-Base = declarative_base()
-
-
-class sheffofprojects(Base):
-    __table__ = Table('sheffofprojects', metadata, autoload=True)
-
-
-Session = sessionmaker(autocommit=True, autoflush=True, bind=engine)
-sessions = Session()
+load_dotenv()
+FULL_PATH_FOLDER = Path(getcwd(), 'static', 'files')
+UPLOAD_FOLDER = Path('/static', 'files')
 app = Flask(__name__)
-app.secret_key = os.urandom(20).hex()
-
-
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    return render_template("index.html")
-
+app.register_blueprint(pages)
+init_db()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=(getenv('PORT') or 5000))
---
+++ models/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+engine = create_engine(getenv('MYSQL'))
+db_session = scoped_session(sessionmaker(autoflush=True, bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+    import models
+    Base.metadata.create_all(bind=engine)
