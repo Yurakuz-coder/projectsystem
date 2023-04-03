04557a07dc004e02f00f3efc3082f5e33807d3df
Yurakuz-coder <61351855+Yurakuz-coder@users.noreply.github.com>
2023-04-03 14:43:24 +0700

    Добавил код в routes.py. Чтение логина и пароля

--- routes.py
+++ routes.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import hashlib
+
 
 pages = Blueprint('pages', __name__, template_folder='templates')
 
-@pages.route('/', methods=['GET'])
+@pages.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+    login = request.form.get['login']
+    password = request.form['pass']
+    passw = hashlib.md5(password.encode())
+    rendered_pass = passw.hexdigest()
+
