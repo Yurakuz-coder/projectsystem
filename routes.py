1022c8aecfd648f713af8e161e2a903747d570bd
Yury Kargin <u.kargin@kmiac.ru>
2023-04-03 17:09:59 +0700

    feat: добавлено взаимодействие с бд

--- routes.py
+++ routes.py
@@ -1,25 +1,29 @@
 from flask import Blueprint, render_template, request, redirect
 import hashlib
 from models.models import *
+from models.database import get_session
 
-pages = Blueprint('pages', __name__, template_folder='templates')
+pages = Blueprint("pages", __name__, template_folder="templates")
 
-@pages.route('/', methods=['GET', 'POST'])
+
+@pages.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         return render_template("index.html")
-    login = request.form['login']
-    password = request.form['pass']
+    login = request.form["login"]
+    password = request.form["pass"]
     passw = hashlib.md5(password.encode())
     rendered_pass = passw.hexdigest()
-    role = request.form['role']
+    role = request.form["role"]
     if role == "sheffofprojects":
-        data_workers = sessions.query(sheffofprojects, positions).join(positions).filter(sheffofprojects.Login == login).first()
-        return redirect('/admin/reg_shefforg')
+        db_session = get_session()
+        data_workers = (
+            db_session.query(Sheffofprojects).filter(Sheffofprojects.Login == login).first()
+        )
+        return redirect("/admin/reg_shefforg")
 
 
-@pages.route('/admin/reg_shefforg', methods=['GET', 'POST'])
+@pages.route("/admin/reg_shefforg", methods=["GET", "POST"])
 def reg_shefforg():
     if request.method == "GET":
         return render_template("registration.html")
-
