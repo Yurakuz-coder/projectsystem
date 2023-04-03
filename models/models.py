df651a7cebda519ef147225e75f54ee2bd5aa90d
Yury Kargin <u.kargin@kmiac.ru>
2023-04-03 17:09:12 +0700

    feat: добавлено описание моделей для взаимодействия с ORM

--- models/models.py
+++ models/models.py
@@ -1,8 +1,26 @@
-from sqlalchemy import *
-
-class sheffofprojects (Base):
-    __table__ = Table('sheffofprojects', metadata, autoload=True)
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, mapped_column, relationship
 
 
-class positions (Base):
-    __table__ = Table('positions', metadata, autoload=True)
+class Base(DeclarativeBase):
+    pass
+
+
+class Sheffofprojects(Base):
+    __tablename__ = "sheffofprojects"
+    IDsheffpr = mapped_column(Integer, primary_key=True)
+    IDpositions = mapped_column(ForeignKey("positions.IDpositions"))
+    sheffprFirstname = mapped_column(String(255), nullable=False)
+    sheffprName = mapped_column(String(255))
+    sheffprFathername = mapped_column(String(255))
+    sheffprPhone = mapped_column(String(12))
+    sheffprEmail = mapped_column(String(100))
+    Login = mapped_column(String(100), nullable=False)
+    Pass = mapped_column(String(100))
+    positionName = relationship("Positions", foreign_keys=[IDpositions])
+
+
+class Positions(Base):
+    __tablename__ = "positions"
+    IDpositions = mapped_column(Integer, primary_key=True)
+    positionsName = mapped_column(String(100), nullable=False)
